4d969d890e4652c79bd9c16d5c6a90bb625175c7
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-12-23 23:01:29 +0500

    add random circlet colors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
 const timeEl = document.querySelector('#time');
-let time = 20;
+const board = document.querySelector('#board');
+let time = 0;
+let score = 0;
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -18,11 +20,18 @@ timeList.addEventListener('click', event => {
     }
 })
 
-startGame()
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++;
+        event.target.remove();
+        createRandomCircle()
+    }
+});
 
 function startGame() {    
-    setInterval(decreaseTime, 1000)
-    setTime(time)
+    setInterval(decreaseTime, 1000);
+    createRandomCircle();
+    setTime(time);
 }
 
 function decreaseTime() {
@@ -43,5 +52,31 @@ function setTime(value) {
 }
 
 function finishGame() {
-    
+    timeEl.parentNode.classList.add('hide');
+    board.innerHTML = `<h1>Ваш счет: <span class="primary">${score}</span></h1>`;
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div');
+    const size = getRandomNumber(10, 60);
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size);
+    const y = getRandomNumber(0, height - size);
+    const colors = ['#FFDAB9', '#66CDAA', '#8B008B', '##C0C0C0', '#ADFF2F', '#8B0000', 'green', 'yellow'];
+    circle.style.top = `${x}px`;
+    circle.style.top = `${x}px`;
+
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+    circle.style.background = colors[Math.floor(Math.random() * colors.length)];
+
+
+    board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
 }
